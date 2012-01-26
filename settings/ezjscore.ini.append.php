6aff2a71f476bfa54ad4259a78f8ddee9a68c6f8
Johan Beronius <johan.beronius@athega.se>
2012-01-26 16:28:54 +0100

    Added CSS optimizer to inline small image files as a Data URL.

---
+++ settings/ezjscore.ini.append.php
@@ -0,0 +1,11 @@
+<?php /* #?ini charset="utf-8"?
+
+[eZJSCore]
+CssOptimizer[]=CssInlineImageOptimizer
+
+[CssInlineImageOptimizer]
+# Maximum filesize for images to be inlined as a Data URL in the CSS.
+# If not set the default value of 2048 will be used.
+#InlineImageMaxBytes=2048
+
+*/ ?>
